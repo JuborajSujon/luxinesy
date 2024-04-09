a8e8db63980de2c129d80438780ef4acaea4396f
Juboraj Sujon <mdsujon72@gmail.com>
2024-04-09 14:38:33 +0600

    define simple routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 import "./index.css";
+import router from "./routes/Routes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <h1 className="text-3xl font-bold underline">LUXINESY</h1>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
