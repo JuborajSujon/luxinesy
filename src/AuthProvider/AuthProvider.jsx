27293672369bdb80ddc5783e1729b91a0a5b5808
Juboraj Sujon <mdsujon72@gmail.com>
2024-04-14 00:33:21 +0600

    create context for firebase data share from any where

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,6 +1,12 @@
 import PropTypes from "prop-types";
+import { createContext } from "react";
+
+export const AuthContext = createContext(null);
 const AuthProvider = ({ children }) => {
-  return <div>{children}</div>;
+  const authInfo = {};
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 AuthProvider.propTypes = {
