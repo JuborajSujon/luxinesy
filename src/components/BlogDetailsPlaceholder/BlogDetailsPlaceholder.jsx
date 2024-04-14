86a271a51267768442f3e78f7e59af1d36316329
Juboraj Sujon <mdsujon72@gmail.com>
2024-04-14 22:26:40 +0600

    Blog details loding placeholder define

---
+++ src/components/BlogDetailsPlaceholder/BlogDetailsPlaceholder.jsx
@@ -0,0 +1,47 @@
+import Container from "../Container/Container";
+
+const BlogDetailsPlaceholder = () => {
+  return (
+    <Container>
+      <div className="px-4">
+        <div className="border border-blue-300 shadow rounded-md  p-4 w-full mx-auto">
+          <div className="animate-pulse">
+            <div className="w-full h-[320px] grid grid-cols-1 md:grid-cols-2 gap-3">
+              <div className="bg-slate-700 h-full rounded"></div>
+              <div className=" h-full grid grid-cols-2 gap-3">
+                <div className="bg-slate-700 "></div>
+                <div className="bg-slate-700 "></div>
+                <div className="bg-slate-700 "></div>
+                <div className="bg-slate-700 "></div>
+              </div>
+            </div>
+            <div className="mt-10 grid grid-cols-1 md:grid-cols-3 md:gap-3 space-y-3 md:space-y-0">
+              <div className="col-span-2 space-y-3">
+                <div className="h-2 bg-slate-700 rounded"></div>
+                <div className="h-2 bg-slate-700 rounded"></div>
+                <div className="h-2 bg-slate-700 rounded"></div>
+                <div className="h-2 bg-slate-700 rounded"></div>
+                <div className="h-2 bg-slate-700 rounded"></div>
+                <div className="h-2 bg-slate-700 rounded"></div>
+                <div className="h-2 bg-slate-700 rounded"></div>
+                <div className="h-2 bg-slate-700 rounded"></div>
+              </div>
+              <div className="space-y-3">
+                <div className="h-2 bg-slate-700 rounded"></div>
+                <div className="h-2 bg-slate-700 rounded"></div>
+                <div className="h-2 bg-slate-700 rounded"></div>
+                <div className="h-2 bg-slate-700 rounded"></div>
+                <div className="h-2 bg-slate-700 rounded"></div>
+                <div className="h-2 bg-slate-700 rounded"></div>
+                <div className="h-2 bg-slate-700 rounded"></div>
+                <div className="h-2 bg-slate-700 rounded"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default BlogDetailsPlaceholder;
--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,5 +1,12 @@
 import PropTypes from "prop-types";
+import useAuth from "../../customHook/useAuth";
+import { Navigate } from "react-router-dom";
 const PrivateRoute = ({ children }) => {
+  const { user } = useAuth();
+
+  if (!user) {
+    return <Navigate to="/login"></Navigate>;
+  }
   return <div>{children}</div>;
 };
 
